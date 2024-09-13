ebe62e6835cf8929e643910d30373e125d345131
Aidenzx <elpanacj64@gmail.com>
2024-09-13 13:34:46 -0500

    Update teldata.js

--- teldata.js
+++ teldata.js
@@ -1,12 +1,12 @@
-
 const axios = require('axios');
 
-const API_KEY = 'c2023659d43ecb2d23519e63db05462e';
-const BASE_URL = 'http://apilayer.net/api/validate';
 
-// Mostrar el título en ASCII art
+const API_KEY = 'num_live_ZAdVEWCqEn8coW65E3HaMAO9jkZsTq91fKB28U8U';
+const BASE_URL = 'https://api.numlookupapi.com/v1/validate';
+
+
 const title = `
- ██████╗███████ ╗██╗     ██████╗  █████╗ ████████╗ █████╗ 
+╔██████═╗██████═╗██╗     ██████╗  █████╗ ████████╗ █████╗ 
 ╚══██╔══╝██╔════╝██║     ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗
    ██║   █████╗  ██║     ██║  ██║███████║   ██║   ███████║
    ██║   ██╔══╝  ██║     ██║  ██║██╔══██║   ██║   ██╔══██║
@@ -16,35 +16,38 @@ const title = `
 
 console.log(title);
 
-// Obtener el número de teléfono y el código de país desde la línea de comandos
+
 const numeroTelefono = process.argv[2];
-const countryCode = process.argv[3] || ''; // Permitir que el usuario ingrese el código de país
 
-// Validar la entrada del número de teléfono
+
 if (!numeroTelefono) {
-    console.log('Uso: node teldata.js <numero_de_telefono> [código_de_país]');
+    console.log('Uso: node teldata.js <numero_de_telefono>');
     process.exit(1);
 }
 
-// Validar el formato del número de teléfono
-const phoneRegex = /^\d+$/;
+
+const phoneRegex = /^\+\d+$/; 
 if (!phoneRegex.test(numeroTelefono)) {
-    console.log('Número de teléfono inválido. Asegúrate de que solo contenga dígitos.');
+    console.log('Número de teléfono inválido. Asegúrate de que contenga el prefijo "+" seguido de dígitos.');
     process.exit(1);
 }
 
-// Construir la URL de la solicitud
-const url = `${BASE_URL}?access_key=${API_KEY}&number=${numeroTelefono}&country_code=${countryCode}&format=1`;
+// Constru
+const url = `${BASE_URL}/${numeroTelefono}?apikey=${API_KEY}`;
 
-// Realizar la solicitud a la API
+// Realiza
 axios.get(url)
     .then(response => {
-        if (response.data.valid) {
+        const data = response.data;
+        if (data.valid) {
             console.log('Información del número de teléfono:');
-            console.log(`Número: ${response.data.number}`);
-            console.log(`Código de país: ${response.data.country_code}`);
-            console.log(`Tipo: ${response.data.line_type}`);
-            console.log(`Operador: ${response.data.carrier}`);
+            console.log(`Número: ${data.intl_format}`);
+            console.log(`Formato local: ${data.local_format}`);
+            console.log(`Nombre del país: ${data.country_name}`);
+            console.log(`Código de país: ${data.country_code}`);
+            console.log(`Tipo: ${data.line_type}`);
+            console.log(`Operador: ${data.carrier}`);
+            console.log(`Ubicación: ${data.location}`);
         } else {
             console.log('Número de teléfono no válido o no encontrado.');
         }
